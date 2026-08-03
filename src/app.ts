800b75ad843fa1fe9141a5687d44c489d3822f5d
Jeffrey <JeffMv@users.noreply.github.com>
2026-08-03 07:21:34 +0200

    test pipeline

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 export const app = express();
 
 app.get("/", (_req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.send("<h1>Hello World 2</h1>");
 });
 
 app.get("/health", (_req, res) => {
